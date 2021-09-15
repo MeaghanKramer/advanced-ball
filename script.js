2bb9031868bd5dd139c58165f2cbdf9297826906
Meaghan Kramer <74553139+MeaghanKramer@users.noreply.github.com>
2021-09-15 10:51:07 +0000

    classes + attempt colour

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-class ball{
-  constructor(x,y,w,h,vx,vy)
-  drawball (){
-  ellipse(x,y,50,50);
-  x = x + v;
-  y = y + z;
-  fill(555,200,400);
-
-  if(y <= 0 || y > 400){
-    z= z * -1;
+class Ball{
+  constructor(x,y,w,h,vx,vy, colour){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.vx = vx;
+    this.vy = vy;
+    this.colour = (random(0,255,777))
   }
 
-  if (x <= 0 || x > 500) {
-    v = v * -1;
+  drawBall (){
+    ellipse(this.x,this.y,50,50); 
+    fill (this.colour);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if(this.y <= 0 || this.y > 500){
+      this.vy= this.vy * -1;
+    }
+
+    if (this.x <= 0 || this.x > 400) {
+      this.vx = this.vx * -1;
+    }
   }
-  }
+}
+
+function setup(){
+  createCanvas(500,500);
+
+  ball1 = new Ball(11,12,30,30,5,5, this.colour);
+  ball2 = new Ball (33,33,60,78,12,12, this.colour);
+}
+
+function draw(){
+  createCanvas(400,400);
+
+  ball1.drawBall();
+  ball2.drawBall();
 }
